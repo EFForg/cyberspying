3a494a10e57298d096a1ed665b371820fa24cff6
Micah Lee <micah@eff.org>
2012-04-18 16:42:34 -0700

    added ACLU instead of just ACLU of Northern Cal

--- js/sidebars.js
+++ js/sidebars.js
@@ -1,6 +1,6 @@
 $(function(){
     // build logos
-    var logos = ['03', '06', '09', '11', '13', '15', '17', '19', '22', '24', '26', 'OC', '28', '36', 'PPF', '37', '30', '38', '39', '32', '35', 'TestPAC'];
+    var logos = ['03', '06', 'ACLU', '11', '13', '15', '17', '19', '22', '24', '26', 'OC', '28', '36', 'PPF', '37', '30', '38', '39', '32', '35', 'TestPAC'];
     $.each(logos, function(){
         var logo_id = this;
         $logo = $('<img src="/images/logos/cispa-1b_'+logo_id+'.jpg">')
